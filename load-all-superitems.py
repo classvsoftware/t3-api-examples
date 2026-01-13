d82cf5c99aa53287797453bb2d0792f3b393200a
Matt Frisbie <msfrisbie@gmail.com>
2026-01-13 16:51:42 -0600

    new items scripts

---
+++ load-all-superitems.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# /// script
+# requires-python = ">=3.8"
+# dependencies = [
+#     "t3api_utils",
+# ]
+# ///
+
+
+from t3api_utils.api.parallel import load_all_data_sync
+from t3api_utils.main.utils import (get_authenticated_client_or_error,
+                                    interactive_collection_handler,
+                                    pick_license)
+
+
+def main():
+    api_client = get_authenticated_client_or_error()
+    
+    license = pick_license(api_client=api_client)
+
+    items = load_all_data_sync(
+        client=api_client,
+        path="/v2/items/super",
+        license_number=license["licenseNumber"],
+        max_workers=2,
+        page_size=50,
+        include="images"
+    )
+    
+    interactive_collection_handler(data=items)
+
+if __name__ == "__main__":
+    main()
